1b92358f8dbe1307d421f631f4481d0861d214ad
UmerKhan1998 <devUk9298@gmail.com>
2025-10-22 21:43:32 -0700

    working database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { MyLoggerModule } from './my-logger/my-logger.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true, // ✅ makes env variables available everywhere
       envFilePath: '.env', // optional (default is .env)
     }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    DatabaseModule,
     ThrottlerModule.forRoot([
       {
         name: 'short',
